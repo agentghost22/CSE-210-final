4a10463f26e28d2dc0adb40e348936ca9837701e
Jeffrey <Agentghost22@gmail.com>
2023-12-11 23:00:29 -0700

    Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,127 @@
 using System;
+using System.Collections.Generic;
+
+class Activity
+{
+    protected DateTime date;
+    protected int minutes;
+
+    public Activity(DateTime date, int minutes)
+    {
+        this.date = date;
+        this.minutes = minutes;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{date:dd MMM yyyy} - {GetType().Name} ({minutes} min) : Distance {GetDistance():F2} miles, Speed {GetSpeed():F2} mph, pace: {GetPace():F2} min per mile";
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0.0;
+    }
+}
+
+class Running : Activity
+{
+    private double distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        this.distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return distance / minutes * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / distance;
+    }
+}
+
+class Cycling : Activity
+{
+    private double speed;
+
+    public Cycling(DateTime date, int minutes, double speed) : base(date, minutes)
+    {
+        this.speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return speed;
+    }
+
+    public override double GetDistance()
+    {
+        return speed * minutes / 60;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / speed;
+    }
+}
+
+class Swimming : Activity
+{
+    private int laps;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        this.laps = laps;
+    }
+// use the right calculations to get swiming correct
+    public override double GetDistance()
+    {
+        return laps * 50.0 / 1000.0 * 0.62;
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / minutes * 60;
+    }
+
+    public override double GetPace()
+    {
+        return minutes / GetDistance();
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 4), 20, 5.0),
+            new Cycling(new DateTime(2022, 11, 5), 30, 6.0),
+            new Swimming(new DateTime(2022, 11, 3), 70, 30),
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
